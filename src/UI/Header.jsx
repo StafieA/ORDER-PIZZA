e784a29831d5e1ef82474151d7d878fdd79f1105
Andrei Stafie <andreistafie@gmail.com>
2025-03-06 14:38:43 +0200

    Build the AppLayout and integrate in the router

---
+++ src/UI/Header.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Stafie's Pizza Company</Link>
+    </header>
+  );
+}
+
+export default Header;
