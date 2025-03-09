015b5a4b189773dbd2ecb2a6016f8b00dfc7575d
Andrei Stafie <andreistafie@gmail.com>
2025-03-09 11:45:02 +0200

    Style text with tailwind

--- src/UI/Header.jsx
+++ src/UI/Header.jsx
@@ -1,12 +1,16 @@
 import { Link } from 'react-router-dom';
 import SearchOrder from '../features/order/SearchOrder';
+import Username from '../features/user/Username';
 
 function Header() {
   return (
-    <header className="bg-yellow-600">
-      <Link to="/">Stafie's Pizza Company</Link>
+    <header className="bg-yellow-600 uppercase">
+      <Link to="/" className="tracking-widest">
+        Stafie's Pizza Company
+      </Link>
 
       <SearchOrder />
+      <Username />
     </header>
   );
 }
