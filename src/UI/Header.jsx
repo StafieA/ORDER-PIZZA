1bb2d1468388bc0b98226f0447b3b1cc969f609c
Andrei Stafie <andreistafie@gmail.com>
2025-03-09 12:24:39 +0200

    Responsive design( addapted to different size screens) with tailwind

--- src/UI/Header.jsx
+++ src/UI/Header.jsx
@@ -4,7 +4,7 @@ import Username from '../features/user/Username';
 
 function Header() {
   return (
-    <header className="border-b-4 border-stone-500 bg-yellow-600 px-4 py-3 uppercase">
+    <header className="border-b-4 border-stone-500 bg-yellow-600 px-4 py-3 uppercase sm:px-6">
       <Link to="/" className="tracking-widest">
         Stafie's Pizza Company
       </Link>
