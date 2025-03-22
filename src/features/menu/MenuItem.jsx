4b81f3d911abf97780ce9a8679c2d897dc60f7aa
Andrei Stafie <andreistafie@gmail.com>
2025-03-22 15:47:37 +0200

    Add menu items to the cart

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,7 +1,10 @@
+import { useDispatch } from 'react-redux';
 import Button from '../../UI/Button';
 import { formatCurrency } from '../../utils/helpers';
+import { addItem } from '../cart/cartSlice';
 
 function MenuItem({ pizza }) {
+  const dispatch = useDispatch();
   const {
     id,
     name,
@@ -11,19 +14,49 @@ function MenuItem({ pizza }) {
     imageUrl,
   } = pizza;
 
+  function handleAddToCart() {
+    const newItem = {
+      pizzaId: id,
+      name,
+      quantity: 1,
+      unitPrice,
+      totalPrice: unitPrice * 1,
+    };
+    dispatch(addItem(newItem));
+  }
+
   return (
-    <li className='flex gap-4 py-2'>
-      <img src={imageUrl} alt={name} className={`h-32 ${soldOut ?'opacity-70 grayscale' :''}`}/>
-      <div className='flex flex-col grow pt-6'>
-        <p className='font-bold text-xl '>{name}</p>
-        <p className='text-sm italic text-stone-500 capitalize'>{ingredients.join(', ')}</p>
-        <div className='mt-auto flex items-center justify-between'>
+    <li className="flex gap-4 py-2">
+      <img
+        src={imageUrl}
+        alt={name}
+        className={`h-32 ${soldOut ? 'opacity-70 grayscale' : ''}`}
+      />
+      <div className="flex grow flex-col pt-6">
+        <p className="text-xl font-bold">
+          {name}
+        </p>
+        <p className="text-sm capitalize italic text-stone-500">
+          {ingredients.join(', ')}
+        </p>
+        <div className="mt-auto flex items-center justify-between">
           {!soldOut ? (
-            <p className='text-sm'>{formatCurrency(unitPrice)}</p>
+            <p className="text-sm">
+              {formatCurrency(unitPrice)}
+            </p>
           ) : (
-            <p className='text-sm uppercase font-medium text-stone-500'>Sold out</p>
+            <p className="text-sm font-medium uppercase text-stone-500">
+              Sold out
+            </p>
           )}
-          <Button type='small'>Add to cart</Button>
+          {!soldOut && (
+            <Button
+              type="small"
+              onClick={handleAddToCart}
+            >
+              Add to cart
+            </Button>
+          )}
         </div>
       </div>
     </li>
